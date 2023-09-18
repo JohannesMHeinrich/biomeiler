2a31bc001370bf307098feb54865581344e6e9c8
johannesmheinrich <johannes.m.heinrich@gmail.com>
2023-09-18 17:12:40 +0200

    testing mqtt 02

--- bm_class_mqtt_publish.py
+++ bm_class_mqtt_publish.py
@@ -18,50 +18,56 @@ import time
 
 
 
-broker = '141.82.98.208'
-port = 1883
-topic = "biomeiler"
+class bm_class_mqtt_publish():
 
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
-username = 'Sonde'
-password = 'Sonde1609'
+    def __init__(self):
 
 
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+        self.broker = '141.82.98.208'
+        self.port = 1883
+        self.topic = "biomeiler"
 
-    client = mqtt_client.Client(client_id)                                  # Set Connecting Client ID
-    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+        # generate client ID with pub prefix randomly
+        self.client_id = f'python-mqtt-{random.randint(0, 1000)}'
+        self.username = 'Sonde'
+        self.password = 'Sonde1609'
 
 
-def publish(client):
-    msg_count = 0
-    while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
+    def connect_mqtt(self):
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
+
+        client = mqtt_client.Client(self.client_id)                                  # Set Connecting Client ID
+        client.username_pw_set(self.username, self.password)
+        client.on_connect = on_connect
+        client.connect(self.broker, self.port)
+        return client
 
 
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publish(client)
+    def publish(self, client):
+        msg_count = 0
+        while True:
+            time.sleep(1)
+            msg = f"messages: {msg_count}"
+            result = client.publish(self.topic, msg)
+            # result: [0, 1]
+            status = result[0]
+            if status == 0:
+                print(f"Send `{msg}` to topic `{self.topic}`")
+            else:
+                print(f"Failed to send message to topic {self.topic}")
+            msg_count += 1
 
 
-if __name__ == '__main__':
-    run()
+    def run(self):
+        client = self.connect_mqtt()
+        client.loop_start()
+        self.publish(client)
+
+
+
+c = bm_class_mqtt_publish
+c.run()
